3a892d1614c1b1e5176d3db70c9f375cb6a40176
midhun <midhunn311@gmail.com>
2022-03-31 21:28:47 +0530

    small updates 👻

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey= '0cfde074e9998f53f87946989161b8922e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: () =>{
+                if(command==='test'){
+                    alert('all set');
+                }
+            }
+        })
+    },[])
     return(
         <div>
             <h1>AI NEWS</h1>
