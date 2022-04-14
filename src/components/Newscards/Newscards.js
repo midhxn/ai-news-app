b6683abd780f00f698e167a1a5120465100b0555
midhun <midhunn311@gmail.com>
2022-04-14 23:06:55 +0530

    commit

--- src/components/Newscards/Newscards.js
+++ src/components/Newscards/Newscards.js
@@ -1,14 +1,21 @@
+import { Grid, Grow, Typography} from "@material-ui/core";
 import React from "react";
 
-import NewsCard from '../Newscard/NewsCard';
+import useStyles from './styles.js';
+import NewsCard from '../NewsCard/NewsCard';
 
 const NewsCards = ({ articles }) => {
+    const classes = useStyles();
     return(
-        <div>
-            {articles.map((articles,i)=>(
-            <NewsCard/> 
-           ))}
-        </div>
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+               {articles.map((article,i)=>(
+                 <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex'}}>
+                      <NewsCard article={article} i={i}/> 
+                 </Grid>     
+               ))}
+            </Grid>
+        </Grow>
         );
 }
 
