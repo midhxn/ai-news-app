468971ae199e31f82c63ff69792ea4c5dbbe927d
midhun <midhunn311@gmail.com>
2022-04-14 23:06:19 +0530

    commit 14/04

---
+++ src/components/Newscards/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles({
+    container: {
+          padding: '0 5%',
+          width: '100%',
+          margin: 0,
+
+        
+    }
+});
