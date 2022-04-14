a87fe16bbe7e6b2bbe07c5a23357720d1d8b54fb
midhun <midhunn311@gmail.com>
2022-04-14 23:06:36 +0530

    commit again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-import NewsCards from "./components/Newscards/Newscards";
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey= '0cfde074e9998f53f87946989161b8922e956eca572e1d8b807a3e2338fdd0dc/stage';
 
@@ -11,19 +11,19 @@ const App = () =>{
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: (command, articles) =>{
+            onCommand: ({command, articles}) =>{
                 if(command==='newHeadlines'){
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
         })
-    },[])
+    }, [])
+
     return(
         <div>
             <h1>AI NEWS</h1>
-            <NewsCards article={newsArticles}/>
+            <NewsCards article={this.newsArticles}/>
         </div>
-
     );
 }
 
