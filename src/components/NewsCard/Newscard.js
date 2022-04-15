2d85222dea52a99882cd3a06ad3617611732e35b
midhun <midhunn311@gmail.com>
2022-04-15 23:38:31 +0530

    commit

--- src/components/NewsCard/Newscard.js
+++ src/components/NewsCard/Newscard.js
@@ -1,8 +1,26 @@
+import { Card, CardActions, CardContent, Button, CardActionArea, CardMedia, Typography } from '@material-ui/core';
 import React from 'react';
 
-const NewsCard = () => {
+const NewsCard = ({article: { description, publishedat, source, title, url
+}}) => {
   return (
-    <div>Newscard</div>
+       <Card>
+         <CardActionArea>
+           <CardMedia/>
+           <div>
+             <Typography variant="body" color="textSecondary" component="h2"></Typography>
+             <Typography variant="body" color="textSecondary" component="h2"></Typography>
+           </div>
+           <Typography gutterBottom variant="h5"></Typography>
+           <CardContent>
+             <Typography variant="body2" color="textSecondary" component="p"></Typography>
+           </CardContent>
+         </CardActionArea>
+         <CardActions>
+           <Button size="small" color="primary"></Button>
+           <Typography variant="h5" color="textSecondary"></Typography>
+         </CardActions>
+       </Card>
   )
 }
 
